50509231ddbd9f55943465647d9d7ee4744f8f71
Anime4000 <ilham92_sakura@yahoo.com>
2019-01-06 00:02:29 +0800

    Create diskspeed.sh

---
+++ bin/diskspeed.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "------------------------------------------"
+echo "|          Disk Read Speed Test          |"
+echo "------------------------------------------"
+echo "           Property of CureComp Technology"
+echo "                      Written by Anime4000"
+
+if [ $# -eq 0 ]; then
+	echo "No arguments provided"
+	echo "with -h to print help and arguments"
+	echo ""
+	exit 1
+fi
+
+if [ "$1" == "-h" ]; then
+	echo "Usage:"
+	echo "diskspeed source_disk"
+	echo ""
+	echo "Examples:"
+	echo "========="
+  echo "diskspeed /dev/sdb1"
+  echo "diskspeed /dev/sdc2"
+	exit 0
+fi
+
+echo "[INFO]: Press CTRL+C to cancel at anytime"
+dd if=$1 of=/dev/null status=progress
