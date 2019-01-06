5a82a1932cea2fb259a522f4637782d407a959e8
Anime4000 <ilham92_sakura@yahoo.com>
2019-01-07 02:53:01 +0800

    Update pmagic_main.sh

--- pmagic/pmagic_main.sh
+++ pmagic/pmagic_main.sh
@@ -56,7 +56,7 @@ cd "$DIR/squashfs-root/usr/share/fonts/TTF/"
 wget --no-check-certificate http://unifoundry.com/pub/unifont/unifont-11.0.03/font-builds/unifont-11.0.03.ttf -O "unifont-11.0.03.ttf"
 wget --no-check-certificate http://www.thescifiworld.net/downloads/fonts/anquietas.ttf -O "anquietas.ttf"
 
-cd "$DIR"
+mkdir -p "$DIR/squashfs-root/etc/xdg/roxterm.sourceforge.net/Profiles/"
 wget --no-check-certificate https://raw.githubusercontent.com/Anime4000/xubuntu-mod/master/pmagic/roxterm-default.conf -O "squashfs-root/etc/xdg/roxterm.sourceforge.net/Profiles/Default"
 wget --no-check-certificate https://raw.githubusercontent.com/Anime4000/xubuntu-mod/master/pmagic/roxterm-ancient.conf -O "squashfs-root/etc/xdg/roxterm.sourceforge.net/Profiles/Ancient"
 
