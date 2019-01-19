01410ff5da06a8ff9ad39d612033985efa40b7b9
Anime4000 <ilham92_sakura@yahoo.com>
2019-01-19 21:40:57 +0800

    Update cclogo.sh

--- bin/cclogo.sh
+++ bin/cclogo.sh
@@ -6,7 +6,7 @@ echo -e "\e[96m / /___/ /_/ / /  /  __/ /___/ / / / / /_/ />  <   / /_/ /___/ / 
 echo -e "\e[96m \____/\__,_/_/   \___/_____/_/_/ /_/\__,_/_/|_|   \____//____/   "
 echo -e "\e[0m"
 echo -e "\e[1;32mCure Linux OS - Data Recovery & Forensic Operating System"
-echo -e "\e[1;32mCopyright (c) 2017 CureComp Technology Trading & Services"
+echo -e "\e[1;32mCopyright (c) 2019 CureComp Technology Trading & Services"
 echo -e ""
 if [ "$EUID" -eq 0 ]; then
 	echo -e "\e[1;91m"
