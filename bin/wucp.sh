b832a1368179f7f133f80ec8382c5667e636eed3
Anime4000 <ilham92_sakura@yahoo.com>
2019-01-19 14:54:36 +0800

    Update wucp.sh

--- bin/wucp.sh
+++ bin/wucp.sh
@@ -39,11 +39,63 @@ if [ "$1" == "-h" ]; then
 	exit 0
 fi
 
+DIR_SOURCE=""
+
+if [ -d "$1/Desktop" ]; then
+	echo "[FOUND]: $1/Desktop"
+	DIR_SOURCE="$DIR_SOURCE \"$1/Desktop\""
+fi
+
+if [ -d "$1/Documents" ]; then
+	echo "[FOUND]: $1/Documents"
+	DIR_SOURCE="$DIR_SOURCE \"$1/Documents\""
+fi
+
+if [ -d "$1/Downloads" ]; then
+	echo "[FOUND]: $1/Downloads"
+	DIR_SOURCE="$DIR_SOURCE \"$1/Downloads\""
+fi
+
+if [ -d "$1/Music" ]; then
+	echo "[FOUND]: $1/Music"
+	DIR_SOURCE="$DIR_SOURCE \"$1/Music\""
+fi
+
+if [ -d "$1/Pictures" ]; then
+	echo "[FOUND]: $1/Pictures"
+	DIR_SOURCE="$DIR_SOURCE \"$1/Pictures\""
+fi
+
+if [ -d "$1/Videos" ]; then
+	echo "[FOUND]: $1/Videos"
+	DIR_SOURCE="$DIR_SOURCE \"$1/Videos\""
+fi
+
+if [ -d "$1/Favorites" ]; then
+	echo "[FOUND]: $1/Favorites"
+	DIR_SOURCE="$DIR_SOURCE \"$1/Favorites\""
+fi
+
+if [ -d "$1/Links" ]; then
+	echo "[FOUND]: $1/Links"
+	DIR_SOURCE="$DIR_SOURCE \"$1/Links\""
+fi
+
+if [ -d "$1/My Documents" ]; then
+	if [ -L "$1/My Documents" ]; then
+		echo "[INFO]: It's symbolic link."
+		echo "[INFO]: Probably Windows Vista & above, Skip!"
+	else
+		echo "[FOUND]: $1/My Documents"
+		DIR_SOURCE="$DIR_SOURCE \"$1/My Documents\""
+	fi
+fi
+
 cd "$PWD"
 mkdir -p "$2/backup_$clock"
-rsync -avh --no-i-r --info=progress2 --exclude-from="/etc/cc/exclude.txt" "$1/Desktop" "$1/Documents" "$1/Downloads" "$1/Music" "$1/Pictures" "$1/Videos" "$1/Favorites" "$1/Links" "$2/backup_$clock"
+rsync -avh --no-i-r --info=progress2 --exclude-from="/etc/cc/exclude.txt" $DIR_SOURCE "$2/backup_$clock"
 
-echo "---------"
-echo " DONE!!! "
-echo "---------"
+echo "-----------"
+echo "| DONE!!! |"
+echo "-----------"
 exit 0
