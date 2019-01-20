4991bb1c2b98b25719a1d4eacdb997616823277a
Anime4000 <ilham92_sakura@yahoo.com>
2019-01-21 01:34:43 +0800

    Update example.sh

--- bootlogo/example.sh
+++ bootlogo/example.sh
@@ -1,6 +1,27 @@
 #!/bin/bash
 
-mkdir bootlogo.dir
-cd bootlogo.dir
-cpio -i < ../bootlogo
-ls | cpio -ov > ../bootlogo.new
+# put this script inside Cubic project folder
+# make sure run outside Cubic chroot
+# make sure run as root
+
+SCRIPT_HOME=`pwd`
+
+wget --no-check-certificate https://github.com/Anime4000/xubuntu-mod/raw/master/bootlogo/splash.png -O "${SCRIPT_HOME}/custom-live-iso/isolinux/splash.png"
+wget --no-check-certificate https://github.com/Anime4000/xubuntu-mod/raw/master/bootlogo/splash.pcx -O "${SCRIPT_HOME}/custom-live-iso/isolinux/splash.pcx"
+
+mv "${SCRIPT_HOME}/custom-live-iso/isolinux/bootlogo" "${SCRIPT_HOME}/custom-live-iso/isolinux/bootlogo.ori"
+
+mkdir "${SCRIPT_HOME}/bootlogo"
+cd "${SCRIPT_HOME}/bootlogo"
+cpio -I < ../custom-live-iso/isolinux/bootlogo.ori
+
+cp -vf "${SCRIPT_HOME}/custom-live-iso/isolinux/splash.png" ./splash.png
+cp -vf "${SCRIPT_HOME}/custom-live-iso/isolinux/splash.pcx" ./splash.pcx
+
+ls | cpio -ov > ../custom-live-iso/isolinux/bootlogo
+
+## Example command
+# mkdir bootlogo.dir
+# cd bootlogo.dir
+# cpio -i < ../bootlogo
+# ls | cpio -ov > ../bootlogo.new
