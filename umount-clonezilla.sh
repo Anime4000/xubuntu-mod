f9676e63fcb627cd0e23109ada9bd2b1908f895b
Anime4000 <ilham92_sakura@yahoo.com>
2019-01-20 12:57:48 +0800

    Update umount-clonezilla.sh

--- umount-clonezilla.sh
+++ umount-clonezilla.sh
@@ -2,10 +2,7 @@
 # Unmount Clonezilla disk" 
 echo 'Unmount clonezilla disk'
 
-devclone1=$(mount | grep '/home' | grep -o -E '(\/dev\/\w+)')
-devclone2=$(mount | grep '/tmp' | grep -o -E '(\/dev\/\w+)')
-
-umount $devclone1
-umount $devclone2
+umount -fdR /home/partimag
+umount -fdR /tmp/ocsroot_bind_root
 
 echo 'DONE! If fails, need run scan disk.'" 
