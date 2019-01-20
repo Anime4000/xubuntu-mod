68193b0e6d6df10d837b98d83aca665763dd4272
Anime4000 <ilham92_sakura@yahoo.com>
2019-01-20 22:20:16 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -54,7 +54,7 @@ add-apt-repository ppa:trebelnik-stefina/i-nex -y
 apt-get update
 apt-get install \
 build-essential git zlib1g-dev libwrap0-dev xfonts-terminus xfonts-unifont screen xmlstarlet python \
-smartmontools gsmartcontrol htop iptraf gcp unzip openvpn qbittorrent i-nex sysbench \
+smartmontools gsmartcontrol htop iptraf gcp unzip openvpn qbittorrent i-nex sysbench linux-generic \
 hdparm drbl clonezilla reiserfsprogs e2fsprogs hfsprogs exfat-utils nilfs-tools pigz \
 gnome-disk-utility gparted gddrescue testdisk recoverjpeg foremost scalpel florence \
 aircrack-ng reaver ettercap-graphical hydra etherape nmap pppoeconf openssh-server -y
