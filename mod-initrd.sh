92ce72856fc4afcfd2330d8ecd8e72c9a255185a
Anime4000 <ilham92_sakura@yahoo.com>
2019-01-20 23:06:18 +0800

    Update mod-initrd.sh

--- mod-initrd.sh
+++ mod-initrd.sh
@@ -6,15 +6,15 @@
 echo "Modify Splash Screen (initramfs)"
 
 SCRIPT_HOME=`pwd`
-INITRD_FILE="$(pwd)/boot/initrd.img-4.18.0-10-generic"
+INITRD_FILE="boot/initrd.img-4.18.0-10-generic"
 
 cd "squashfs-root"
 
 mkdir -p tmp/000-initrd
 unmkinitramfs ${INITRD_FILE} tmp/000-initrd
-cd tmp/000-initrd/main
-wget --no-check-certificate https://raw.githubusercontent.com/Anime4000/xubuntu-mod/master/plymouth/logo.png -O "usr/share/plymouth/themes/xubuntu-logo/logo.png"
-wget --no-check-certificate https://raw.githubusercontent.com/Anime4000/xubuntu-mod/master/plymouth/logo_16bit.png -O "usr/share/plymouth/themes/xubuntu-logo/logo_16bit.png"
-wget --no-check-certificate https://raw.githubusercontent.com/Anime4000/xubuntu-mod/master/plymouth/wallpaper.png -O "usr/share/plymouth/themes/xubuntu-logo/wallpaper.png"
-cd ../
+
+wget --no-check-certificate https://raw.githubusercontent.com/Anime4000/xubuntu-mod/master/plymouth/logo.png -O "tmp/000-initrd/main/usr/share/plymouth/themes/xubuntu-logo/logo.png"
+wget --no-check-certificate https://raw.githubusercontent.com/Anime4000/xubuntu-mod/master/plymouth/logo_16bit.png -O "tmp/000-initrd/main/usr/share/plymouth/themes/xubuntu-logo/logo_16bit.png"
+wget --no-check-certificate https://raw.githubusercontent.com/Anime4000/xubuntu-mod/master/plymouth/wallpaper.png -O "tmp/000-initrd/main/usr/share/plymouth/themes/xubuntu-logo/wallpaper.png"
+
 mkinitramfs -o ${INITRD_FILE}
