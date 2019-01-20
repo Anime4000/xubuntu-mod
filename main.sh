5af80df1c377b61d65137121682c7631ce2b5193
Anime4000 <ilham92_sakura@yahoo.com>
2019-01-20 22:00:13 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -238,6 +238,7 @@ cd /tmp/000-initrd/main
 wget --no-check-certificate https://raw.githubusercontent.com/Anime4000/xubuntu-mod/master/plymouth/logo.png -O "usr/share/plymouth/themes/xubuntu-logo/logo.png"
 wget --no-check-certificate https://raw.githubusercontent.com/Anime4000/xubuntu-mod/master/plymouth/logo_16bit.png -O "usr/share/plymouth/themes/xubuntu-logo/logo_16bit.png"
 wget --no-check-certificate https://raw.githubusercontent.com/Anime4000/xubuntu-mod/master/plymouth/wallpaper.png -O "usr/share/plymouth/themes/xubuntu-logo/wallpaper.png"
+cd /tmp/000-initrd
 mkinitramfs -o ${INITRD_FILE}
 
 echo "Clean temporary files"
