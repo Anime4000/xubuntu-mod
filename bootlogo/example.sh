d84f70047b4266ed84a5625d172fc73c55848164
Anime4000 <ilham92_sakura@yahoo.com>
2019-01-21 01:43:54 +0800

    Update example.sh

--- bootlogo/example.sh
+++ bootlogo/example.sh
@@ -9,16 +9,15 @@ SCRIPT_HOME=`pwd`
 wget --no-check-certificate https://github.com/Anime4000/xubuntu-mod/raw/master/bootlogo/splash.png -O "${SCRIPT_HOME}/custom-live-iso/isolinux/splash.png"
 wget --no-check-certificate https://github.com/Anime4000/xubuntu-mod/raw/master/bootlogo/splash.pcx -O "${SCRIPT_HOME}/custom-live-iso/isolinux/splash.pcx"
 
-mv "${SCRIPT_HOME}/custom-live-iso/isolinux/bootlogo" "${SCRIPT_HOME}/custom-live-iso/isolinux/bootlogo.ori"
-
 rm -rf "${SCRIPT_HOME}/bootlogo"
 mkdir "${SCRIPT_HOME}/bootlogo"
 cd "${SCRIPT_HOME}/bootlogo"
-cpio -i < ../custom-live-iso/isolinux/bootlogo.ori
+cpio -i < ../original-iso-mount/isolinux/bootlogo
 
 cp -vf "${SCRIPT_HOME}/custom-live-iso/isolinux/splash.png" ./splash.png
 cp -vf "${SCRIPT_HOME}/custom-live-iso/isolinux/splash.pcx" ./splash.pcx
 
+rm -f ../custom-live-iso/isolinux/bootlogo
 ls | cpio -ov > ../custom-live-iso/isolinux/bootlogo
 
 ## Example command
