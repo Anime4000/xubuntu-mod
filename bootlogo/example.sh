341f92ee4ea79cdb604d836529edd6f46af97bf1
Anime4000 <ilham92_sakura@yahoo.com>
2019-01-22 13:23:04 +0800

    Update example.sh

--- bootlogo/example.sh
+++ bootlogo/example.sh
@@ -9,16 +9,22 @@ SCRIPT_HOME=`pwd`
 wget --no-check-certificate https://github.com/Anime4000/xubuntu-mod/raw/master/bootlogo/splash.png -O "${SCRIPT_HOME}/custom-live-iso/isolinux/splash.png"
 wget --no-check-certificate https://github.com/Anime4000/xubuntu-mod/raw/master/bootlogo/splash.pcx -O "${SCRIPT_HOME}/custom-live-iso/isolinux/splash.pcx"
 
-rm -rf "${SCRIPT_HOME}/bootlogo"
-mkdir "${SCRIPT_HOME}/bootlogo"
-cd "${SCRIPT_HOME}/bootlogo"
+rm -f "${SCRIPT_HOME}/bootlogo.new"
+
+rm -rf "${SCRIPT_HOME}/bootlogo.data"
+mkdir -p "${SCRIPT_HOME}/bootlogo.data"
+cd "${SCRIPT_HOME}/bootlogo.data"
+
 cpio -i < ../original-iso-mount/isolinux/bootlogo
 
 cp -vf "${SCRIPT_HOME}/custom-live-iso/isolinux/splash.png" ./splash.png
 cp -vf "${SCRIPT_HOME}/custom-live-iso/isolinux/splash.pcx" ./splash.pcx
 
-rm -f ../custom-live-iso/isolinux/bootlogo
-ls | cpio -ov > ../custom-live-iso/isolinux/bootlogo
+ls | cpio -ov > ../bootlogo.new
+
+echo "OKAY!"
+sleep 3s
+exit 0
 
 ## Example command
 # mkdir bootlogo.dir
